e513d7f5c2e89bd2db086b4aab8361ebc353f0c4
Gerson121295 <Gerson21millones@gmail.com>
2023-05-16 09:58:10 -0600

    2 - Variables

---
+++ src/Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+//En un programa pueden haber muchos metodos main pero por buena practica solo 1
+    public static void main(String[] args) {
+        //Declarando una variable
+        int speed;
+        //Asigna valor a la variable
+        speed = 10;
+        System.out.println(speed);
+
+        int salary = 1000;
+
+        String employeeName = "Gerson";
+        System.out.println(employeeName);
+    }
+}
