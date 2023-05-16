cd77f0fd91cfc9d07a74bb2d6270fd46956cf496
Gerson121295 <Gerson21millones@gmail.com>
2023-05-16 10:40:11 -0600

    4 - Convencion para usar variables

--- src/Variables.java
+++ src/Variables.java
@@ -11,5 +11,24 @@ public class Variables {
 
         String employeeName = "Gerson";
         System.out.println(employeeName);
+
+        //Naming- Convencion de Nombres para usar variables: Ej: camelCase
+        //Inciacion de variables validas
+//       String $countryName = "Spain";
+//        String _backgroundColor = "Green";
+
+//Inciacion de variables No validas
+//        String background-color = "blue";
+
+        //Constantes
+        int POSITION = -5;
+        int MAX_WIDTH = 99;
+        int MIN_WIDTH = 1;
+
+        //Convención:
+        // Upper Camel Case: NamingJava (para nombre de Clases)
+        // Lower Camel Case: fullName, sizeInCentimeters (para nombre de variables y metodos)
+
     }
 }
+
