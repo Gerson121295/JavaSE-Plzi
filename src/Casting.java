c2aed651b22c41a07bec2a59bdc9236344d3b2ed
Gerson121295 <Gerson21millones@gmail.com>
2023-05-17 09:56:17 -0600

    10 - Casteo automatico

--- src/Casting.java
+++ src/Casting.java
@@ -14,7 +14,20 @@ public class Casting {
         int a = 30;
         int b = 12;
 
-        System.out.println((double) a/b);
+        System.out.println((double) a/b); //Resultado es: 2.5
+        //El codigo anterior el casting es forzado
+
+        //Casting automatico deja a criterio el resultado
+        double c = a/b;
+        System.out.println(c); //Resultado es: 2.0
+
+        //Convertir un caracter a entero
+        char n = '1';
+        int nI= n;
+        System.out.println(nI); // 49
+
+        short nS =(short) n;
+        System.out.println(nS);
 
     }
 }
