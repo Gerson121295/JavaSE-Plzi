951d55ce62f7cdb51602a9a59243c9d59fc9a6b2
Gerson121295 <Gerson21millones@gmail.com>
2023-05-17 10:48:14 -0600

    12 - else de la condicion if

--- src/IfStatement.java
+++ src/IfStatement.java
@@ -1,6 +1,6 @@
 public class IfStatement {
     public static void main(String[] args) {
-        boolean isBluetoothEnabled = true;
+        boolean isBluetoothEnabled = false;
         int fileSended = 3;
 
         if(isBluetoothEnabled){ //Si es verdadero
@@ -8,6 +8,11 @@ public class IfStatement {
             fileSended++;
             System.out.println("archivo Enviado");
             System.out.println(fileSended);
+        }else{
+            fileSended--;
+            System.out.println("Please turn on your bluetooth to start the transfer");
+            System.out.println(fileSended);
         }
+
     }
 }
