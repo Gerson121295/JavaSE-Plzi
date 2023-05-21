fcf81092ec0e7caa768331ffb09b04c8705a21c9
Gerson121295 <Gerson21millones@gmail.com>
2023-05-21 11:20:14 -0600

    21 - Declarar arreglos de 1,2,3y4 dimensiones

---
+++ src/Arrays.java
@@ -0,0 +1,22 @@
+public class Arrays {
+    public static void main(String[] args) {
+        //Declarar un Array
+        String[] androidVersions = new String[17];
+        String days[] = new String[7];
+        String[][] cities = new String[4][2]; //4*2=8(le caben 8 elementos)
+
+        /*
+        * -----------------------------------
+        * |     Country     |   City        |
+        * -----------------------------------
+        * |     Mexico      |   CDMX        |
+        * |     Mexico      |   Guadalajara |
+        * |     Colombia    |   Bogota      |
+        * |     Colombia    |   Medellin    |
+        * -----------------------------------
+         */
+       int [][][] numbers = new int [2][2][2]; //2 renglones, 2 columnas y 2 paginas
+        int [][][][] numbers4 = new int [2][2][2][2];
+
+    }
+}
